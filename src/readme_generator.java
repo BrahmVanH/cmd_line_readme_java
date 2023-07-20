14e38b0c0e662722a9a4339a144be829d0332dfa
Brahm V <brahmvanh@gmail.com>
2023-07-19 20:50:01 -0400

    First commit

---
+++ src/readme_generator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class readme_generator {
+
+    public void main(String args[]) {
+        Scanner prompt = new Scanner(System.in);
+        String githubUsername;
+        String email;
+        String title;
+        String description;
+
+    }
+}
